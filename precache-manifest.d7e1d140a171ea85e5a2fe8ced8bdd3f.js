35b5cf7a0decfcd090eda03c581eb25d4342597f
Alex <2nd.runnr@gmail.com>
2019-10-15 07:22:40 -0700

    Updates

---
+++ precache-manifest.d7e1d140a171ea85e5a2fe8ced8bdd3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4271eefdb3323daa8b1365c2f3fd0f",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "a380bf9ca85ddbe63b16",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4b5880bbc357f5da2d30",
+    "url": "/robotfriends/static/css/main.95bb5d21.chunk.css"
+  },
+  {
+    "revision": "a380bf9ca85ddbe63b16",
+    "url": "/robotfriends/static/js/2.1cc82ed5.chunk.js"
+  },
+  {
+    "revision": "4b5880bbc357f5da2d30",
+    "url": "/robotfriends/static/js/main.bd5a9aba.chunk.js"
+  },
+  {
+    "revision": "d9953065c9c3306ad6b5",
+    "url": "/robotfriends/static/js/runtime-main.08a6fd18.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
